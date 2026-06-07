9b3a86482227efe1547ba59ff1b9e64bcabe6ae8
Flowplan-reform <romina.vella96@gmail.com>
2026-06-07 13:21:49 +0200

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../reforma-app.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import App from './reforma-app.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
